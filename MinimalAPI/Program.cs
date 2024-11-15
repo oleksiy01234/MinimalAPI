5878ee64bc8ca73082bc3af1f38759c8e6dfa4b7
Oleksiy Dobrodum <odobrodum@microsoft.com>
2024-11-15 10:34:30 -0500

    Simplified the app to return "hello world"

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -27,24 +27,10 @@ namespace MinimalAPI
 
             app.UseAuthorization();
 
-            var summaries = new[]
+            app.MapGet("/hello_world", (HttpContext httpContext) =>
             {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
+                return "Hello World!";
             })
-            .WithName("GetWeatherForecast")
             .WithOpenApi();
 
             app.Run();
