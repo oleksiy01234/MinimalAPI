dfd7ac9f5b3de74796b6670d86538bb09abee787
Oleksiy Dobrodum <odobrodum@microsoft.com>
2024-11-15 10:59:30 -0500

    Update return to test

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -7,7 +7,7 @@ namespace MinimalAPI
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World! This is a test.");
             app.Run();
         }
     }
